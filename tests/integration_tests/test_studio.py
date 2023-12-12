af282ba93fbc28677cdeac3fae5ba675af3eae7e
Asaf Gardin <asafg@ai21.com>
2023-12-12 13:09:02 +0200

    test: removed example script for studio and added integration test instead

---
+++ tests/integration_tests/test_studio.py
@@ -0,0 +1,54 @@
+"""
+Run this script after setting the environment variable called API_KEY
+"""
+
+import os
+import pytest
+import subprocess
+
+
+@pytest.mark.parametrize(
+    argnames=["test_file_name"],
+    argvalues=[
+        ("answer.py",),
+        ("chat.py",),
+        ("completion.py",),
+        ("embed.py",),
+        ("gec.py",),
+        ("improvements.py",),
+        ("paraphrase.py",),
+        ("segmentation.py",),
+        ("summarize.py",),
+        ("summarize_by_segment.py",),
+        ("tokenization.py",),
+        # ("custom_model.py", ),
+        # ('custom_model_completion.py', ),
+        # ("dataset.py", ),
+        # ("library.py", ),
+        # ("library_answer.py", ),
+    ],
+    ids=[
+        "when_answer__should_return_ok",
+        "when_chat__should_return_ok",
+        "when_completion__should_return_ok",
+        "when_embed__should_return_ok",
+        "when_gec__should_return_ok",
+        "when_improvements__should_return_ok",
+        "when_paraphrase__should_return_ok",
+        "when_segmentation__should_return_ok",
+        "when_summarize__should_return_ok",
+        "when_summarize_by_segment__should_return_ok",
+        "when_tokenization__should_return_ok",
+        # "when_custom_model__should_return_ok",
+        # "when_custom_model_completion__should_return_ok",
+        # "when_dataset__should_return_ok",
+        # "when_library__should_return_ok",
+        # "when_library_answer__should_return_ok",
+    ],
+)
+def test_studio(test_file_name: str):
+    current_dir = os.getcwd()
+    file_path = os.path.join(current_dir, "../../examples", test_file_name)
+    print(f"About to run: {file_path}")
+    exit_code = subprocess.call(["python", file_path])
+    assert exit_code == 0, f"failed to run {test_file_name}"
