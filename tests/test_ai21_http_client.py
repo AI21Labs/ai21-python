c469376addd02da1906284272f2a5945742a2aed
Asaf Gardin <asafg@ai21.com>
2023-12-24 18:10:52 +0200

    test: Added tests

--- tests/test_ai21_http_client.py
+++ tests/test_ai21_http_client.py
@@ -7,6 +7,16 @@ from ai21.ai21_http_client import AI21HTTPClient
 from ai21.version import VERSION
 
 _DUMMY_API_KEY = "dummy_key"
+_EXPECTED_GET_HEADERS = {
+    "Authorization": "Bearer dummy_key",
+    "Content-Type": "application/json",
+    "User-Agent": f"ai21 studio SDK {VERSION}",
+}
+
+_EXPECTED_POST_FILE_HEADERS = {
+    "Authorization": "Bearer dummy_key",
+    "User-Agent": f"ai21 studio SDK {VERSION}",
+}
 
 
 class MockResponse:
@@ -69,29 +79,56 @@ class TestAI21StudioClient:
         client = AI21HTTPClient(api_key=_DUMMY_API_KEY, api_host=api_host, api_version="v1")
         assert client.get_base_url() == expected_api_host
 
-    def test__execute_http_request__when_making_request__should_send_appropriate_parameters(
+    @pytest.mark.parametrize(
+        ids=[
+            "when_making_request__should_send_appropriate_parameters",
+            "when_making_request_with_files__should_send_appropriate_post_request",
+        ],
+        argnames=["params", "headers"],
+        argvalues=[
+            ({"method": "GET", "url": "test_url", "params": {"foo": "bar"}}, _EXPECTED_GET_HEADERS),
+            (
+                {"method": "POST", "url": "test_url", "params": {"foo": "bar"}, "files": {"file": "test_file"}},
+                _EXPECTED_POST_FILE_HEADERS,
+            ),
+        ],
+    )
+    def test__execute_http_request__(
+        self,
+        params,
+        headers,
+        dummy_api_host: str,
+        mock_requests_session: requests.Session,
+    ):
+        response_json = {"test_key": "test_value"}
+
+        client = AI21HTTPClient(api_key=_DUMMY_API_KEY, api_host=dummy_api_host, api_version="v1")
+        mock_requests_session.request.return_value = MockResponse(response_json, 200)
+        response = client.execute_http_request(session=mock_requests_session, **params)
+        assert response == response_json
+
+        if "files" in params:
+            # We split it because when calling requests with "files", "params" is turned into "data"
+            mock_requests_session.request.assert_called_once_with(
+                timeout=300,
+                headers=headers,
+                files=params["files"],
+                data=params["params"],
+                url=params["url"],
+                method=params["method"],
+            )
+        else:
+            mock_requests_session.request.assert_called_once_with(timeout=300, headers=headers, **params)
+
+    def test__execute_http_request__when_files_with_put_method__should_raise_value_error(
         self,
         dummy_api_host: str,
         mock_requests_session: requests.Session,
     ):
-        method = "GET"
-        url = "test_url"
-        params = {"foo": "bar"}
         response_json = {"test_key": "test_value"}
-        headers = {
-            "Authorization": "Bearer dummy_key",
-            "Content-Type": "application/json",
-            "User-Agent": f"ai21 studio SDK {VERSION}",
-        }
 
         client = AI21HTTPClient(api_key=_DUMMY_API_KEY, api_host=dummy_api_host, api_version="v1")
         mock_requests_session.request.return_value = MockResponse(response_json, 200)
-        response = client.execute_http_request(method=method, url=url, params=params, session=mock_requests_session)
-        assert response == response_json
-        mock_requests_session.request.assert_called_once_with(
-            method,
-            url,
-            headers=headers,
-            timeout=300,
-            params=params,
-        )
+        with pytest.raises(ValueError):
+            params = {"method": "PUT", "url": "test_url", "params": {"foo": "bar"}, "files": {"file": "test_file"}}
+            client.execute_http_request(session=mock_requests_session, **params)
