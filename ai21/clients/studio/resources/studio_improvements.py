147fc831715672f9a0008a803210a1409ca4fcfd
Asaf Gardin <asafg@ai21.com>
2023-12-28 15:19:43 +0200

    refactor: Added improvement type enum

--- ai21/clients/studio/resources/studio_improvements.py
+++ ai21/clients/studio/resources/studio_improvements.py
@@ -2,12 +2,13 @@ from typing import List
 
 from ai21.errors import EmptyMandatoryListError
 from ai21.resources.bases.improvements_base import Improvements
+from ai21.resources.models.improvement_type import ImprovementType
 from ai21.resources.responses.improvement_response import ImprovementsResponse
 from ai21.resources.studio_resource import StudioResource
 
 
 class StudioImprovements(StudioResource, Improvements):
-    def create(self, text: str, types: List[str], **kwargs) -> ImprovementsResponse:
+    def create(self, text: str, types: List[ImprovementType], **kwargs) -> ImprovementsResponse:
         if len(types) == 0:
             raise EmptyMandatoryListError("types")
 
--- ai21/resources/__init__.py
+++ ai21/resources/__init__.py
@@ -1,7 +1,8 @@
 from ai21.resources.bases.answer_base import AnswerLength, Mode
 from ai21.resources.bases.chat_base import Message
+from ai21.resources.models.improvement_type import ImprovementType
 from ai21.resources.models.penalty import Penalty
 from ai21.resources.models.role_type import RoleType
 from ai21.resources.bases.embed_base import EmbedType
 
-__all__ = ["AnswerLength", "Mode", "Message", "RoleType", "Penalty", "EmbedType"]
+__all__ = ["AnswerLength", "Mode", "Message", "RoleType", "Penalty", "EmbedType", "ImprovementType"]
