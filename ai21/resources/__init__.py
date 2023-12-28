cb496a9c593ee4c74c2a39d1faa839e6790159ad
Asaf Gardin <asafg@ai21.com>
2023-12-28 15:07:09 +0200

    refactor: Added embed types enum

--- ai21/resources/__init__.py
+++ ai21/resources/__init__.py
@@ -2,6 +2,6 @@ from ai21.resources.bases.answer_base import AnswerLength, Mode
 from ai21.resources.bases.chat_base import Message
 from ai21.resources.models.penalty import Penalty
 from ai21.resources.models.role_type import RoleType
+from ai21.resources.bases.embed_base import EmbedType
 
-
-__all__ = ["AnswerLength", "Mode", "Message", "RoleType", "Penalty"]
+__all__ = ["AnswerLength", "Mode", "Message", "RoleType", "Penalty", "EmbedType"]
--- ai21/resources/bases/embed_base.py
+++ ai21/resources/bases/embed_base.py
@@ -1,14 +1,20 @@
 from abc import ABC, abstractmethod
+from enum import Enum
 from typing import List, Any, Dict, Optional
 
 from ai21.resources.responses.embed_response import EmbedResponse
+
+
+class EmbedType(str, Enum):
+    QUERY = "query"
+    SEGMENT = "segment"
 
 
 class Embed(ABC):
     _module_name = "embed"
 
     @abstractmethod
-    def create(self, texts: List[str], *, type: Optional[str] = None, **kwargs) -> EmbedResponse:
+    def create(self, texts: List[str], *, type: Optional[EmbedType] = None, **kwargs) -> EmbedResponse:
         pass
 
     def _json_to_response(self, json: Dict[str, Any]) -> EmbedResponse:
