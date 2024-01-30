e7fff8081036c9c9316ca45389a8f80cc910314a
asafgardin <147075902+asafgardin@users.noreply.github.com>
2024-01-30 18:04:02 +0200

    test: Added tests for library (#45)
    * test: Added tests for library
    
    * fix: CR

---
+++ tests/integration_tests/clients/studio/test_library.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+from time import sleep
+
+from ai21 import AI21Client
+from tests.integration_tests.clients.studio.conftest import LIBRARY_FILE_TO_UPLOAD, DEFAULT_LABELS
+
+
+def test_library__when_upload__should_get_file_id(file_in_library: str):
+    assert file_in_library is not None
+
+
+def test_library__when_list__should_get_file_id_in_list_of_files(file_in_library: str):
+    client = AI21Client()
+
+    files = client.library.files.list()
+    assert files[0].file_id == file_in_library
+    assert files[0].name == Path(LIBRARY_FILE_TO_UPLOAD).name
+
+
+def test_library__when_get__should_match_file_id(file_in_library: str):
+    client = AI21Client()
+
+    file_response = client.library.files.get(file_in_library)
+    assert file_response.file_id == file_in_library
+
+
+def test_library__when_update__should_update_labels_successfully(file_in_library: str):
+    client = AI21Client()
+
+    file_response = client.library.files.get(file_in_library)
+    assert set(file_response.labels) == set(DEFAULT_LABELS)
+    sleep(2)
+
+    new_labels = DEFAULT_LABELS + ["new_label"]
+    client.library.files.update(file_in_library, labels=new_labels)
+    file_response = client.library.files.get(file_in_library)
+    assert set(file_response.labels) == set(new_labels)
