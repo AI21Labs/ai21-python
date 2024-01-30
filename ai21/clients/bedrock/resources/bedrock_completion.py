127cef460d295b7d7a3af83848f30403cf04ba4b
asafgardin <147075902+asafgardin@users.noreply.github.com>
2024-01-30 09:57:09 +0200

    fix: aws tests (#44)
    * ci: rc branch trigger for integration test
    
    * fix: wrapped in quotes
    
    * fix: AWS tests
    
    * test: ci
    
    * fix: AWS tests
    
    * test: ci
    
    * fix: Removed testing pattern for tests

--- ai21/clients/bedrock/resources/bedrock_completion.py
+++ ai21/clients/bedrock/resources/bedrock_completion.py
@@ -1,7 +1,7 @@
 from typing import Optional, List
 
-from ai21.resources import Penalty
 from ai21.clients.bedrock.resources.bedrock_resource import BedrockResource
+from ai21.models import Penalty
 from ai21.models.responses.completion_response import CompletionsResponse
 
 
