e5d85ffd503f2b973a5ecaa872c33efb2be60149
Asaf Gardin <asafg@ai21.com>
2024-02-25 08:44:31 +0200

    refactor: rename test params

--- tests/unittests/clients/bedrock/resources/test_completion.py
+++ tests/unittests/clients/bedrock/resources/test_completion.py
@@ -7,8 +7,8 @@ from unittest.mock import Mock, ANY
 from ai21.clients.bedrock.resources.bedrock_completion import BedrockCompletion
 from ai21.models import Prompt
 
-_TEST_MODEL_ID = "test_model_id"
-_TEST_NEW_MODEL_ID = "new_model_id"
+_CTOR_PROVIDED_MODEL_ID = "constructor_provided_model_id"
+_INVOCATION_MODEL_ID = "invocation_model_id"
 
 
 @pytest.mark.parametrize(
@@ -16,14 +16,14 @@ _TEST_NEW_MODEL_ID = "new_model_id"
         "when_model_id_not_passed_in_create__should_use_model_id_from_init",
         "when_model_id_passed_in_create__should_use_model_id_from_create",
     ],
-    argnames=["new_model_id", "expected_model_id"],
+    argnames=["invocation_model_id", "expected_model_id"],
     argvalues=[
-        (None, _TEST_MODEL_ID),
-        (_TEST_NEW_MODEL_ID, _TEST_NEW_MODEL_ID),
+        (None, _CTOR_PROVIDED_MODEL_ID),
+        (_INVOCATION_MODEL_ID, _INVOCATION_MODEL_ID),
     ],
 )
 def test__when_model_id_create_and_init__should_use_one_from_create(
-    new_model_id: Optional[str],
+    invocation_model_id: Optional[str],
     expected_model_id: str,
     mock_bedrock_session: Mock,
     mocker: MockerFixture,
@@ -34,12 +34,12 @@ def test__when_model_id_create_and_init__should_use_one_from_create(
         "completions": [],
     }
 
-    client = BedrockCompletion(model_id=_TEST_MODEL_ID, bedrock_session=mock_bedrock_session)
+    client = BedrockCompletion(model_id=_CTOR_PROVIDED_MODEL_ID, bedrock_session=mock_bedrock_session)
 
     # We can not pass "None" explicitly to the create method, so we have to use the if else statement
-    if new_model_id is None:
+    if invocation_model_id is None:
         client.create(prompt="test")
     else:
-        client.create(model_id=new_model_id, prompt="test")
+        client.create(model_id=invocation_model_id, prompt="test")
 
     mock_bedrock_session.invoke_model.assert_called_once_with(model_id=expected_model_id, input_json=ANY)
