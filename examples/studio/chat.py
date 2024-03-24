84a99e5d560c569e20d6a6e72bac761598818bbe
Asaf Gardin <asafg@ai21.com>
2024-03-24 17:12:36 +0200

    fix: __call__ to __post_init__

--- examples/studio/chat.py
+++ examples/studio/chat.py
@@ -3,16 +3,16 @@ from ai21.models import ChatMessage, RoleType, Penalty
 
 system = "You're a support engineer in a SaaS company"
 messages = [
-    ChatMessage(text="Hello, I need help with a signup process.", role=RoleType.USER),
-    ChatMessage(text="Hi Alice, I can help you with that. What seems to be the problem?", role=RoleType.ASSISTANT),
-    ChatMessage(text="I am having trouble signing up for your product with my Google account.", role=RoleType.USER),
+    ChatMessage(content="Hello, I need help with a signup process.", role=RoleType.USER),
+    ChatMessage(content="Hi Alice, I can help you with that. What seems to be the problem?", role=RoleType.ASSISTANT),
+    ChatMessage(content="I am having trouble signing up for your product with my Google account.", role=RoleType.USER),
 ]
 
 client = AI21Client()
 response = client.chat.create(
     system=system,
     messages=messages,
-    model="j2-ultra",
+    model="j2-mid",
     count_penalty=Penalty(
         scale=0,
         apply_to_emojis=False,
