d200d0edff8d3d441829c45f534b25927dbd7a4f
Asaf Gardin <asafg@ai21.com>
2024-03-26 15:32:51 +0200

    fix: all import

--- ai21/models/chat/__init__.py
+++ ai21/models/chat/__init__.py
@@ -4,3 +4,5 @@ from .chat_completion_response import ChatCompletionResponse
 from .chat_completion_response import ChatCompletionResponseChoice
 from .chat_message import ChatMessage
 from .role_type import RoleType as RoleType
+
+__all__ = ["ChatCompletionResponse", "ChatCompletionResponseChoice", "ChatMessage", "RoleType"]
--- ai21/models/chat/role_type.py
+++ ai21/models/chat/role_type.py
@@ -1,7 +1,5 @@
 from enum import Enum
 
-__all__ = ["RoleType"]
-
 
 class RoleType(str, Enum):
     USER = "user"
