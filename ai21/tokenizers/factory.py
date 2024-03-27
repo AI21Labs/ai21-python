5b7faa5e77b209a2c2e978d85829e5dae9f2517c
asafg <asafg@ai21.com>
2024-03-27 14:28:58 +0200

    feat: support get_tokenizer by tokenizer name

--- ai21/tokenizers/factory.py
+++ ai21/tokenizers/factory.py
@@ -1,21 +1,21 @@
-from typing import Optional
+from typing import Dict
 
-from ai21_tokenizer import Tokenizer
+from ai21_tokenizer import Tokenizer, PreTrainedTokenizers
 
 from ai21.tokenizers.ai21_tokenizer import AI21Tokenizer
 
-_cached_tokenizer: Optional[AI21Tokenizer] = None
+_cached_tokenizers: Dict[str, AI21Tokenizer] = {}
 
 
-def get_tokenizer() -> AI21Tokenizer:
+def get_tokenizer(tokenizer_name: str = PreTrainedTokenizers.J2_TOKENIZER) -> AI21Tokenizer:
     """
     Get the tokenizer instance.
 
     If the tokenizer instance is not cached, it will be created using the Tokenizer.get_tokenizer() method.
     """
-    global _cached_tokenizer
+    global _cached_tokenizers
 
-    if _cached_tokenizer is None:
-        _cached_tokenizer = AI21Tokenizer(Tokenizer.get_tokenizer())
+    if _cached_tokenizers.get(tokenizer_name) is None:
+        _cached_tokenizers[tokenizer_name] = AI21Tokenizer(Tokenizer.get_tokenizer(tokenizer_name))
 
-    return _cached_tokenizer
+    return _cached_tokenizers[tokenizer_name]
