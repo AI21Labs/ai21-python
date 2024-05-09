3401934daba4a8d2ad7d99a11ed25fe87db26b78
Josephasafg <ajgard7@gmail.com>
2024-05-09 09:06:55 +0300

    fix: Modified test

--- tests/unittests/test_http_client.py
+++ tests/unittests/test_http_client.py
@@ -4,8 +4,8 @@ from urllib.request import Request
 
 import httpx
 
-from ai21 import TooManyRequestsError
-from ai21.http_client import HttpClient
+from ai21.errors import ServiceUnavailable
+from ai21.http_client import HttpClient, RETRY_ERROR_CODES
 
 _METHOD = "GET"
 _URL = "http://test_url"
@@ -26,15 +26,14 @@ def test__execute_http_request__when_retry_error_code_once__should_retry_and_suc
 
 def test__execute_http_request__when_retry_error__should_retry_and_stop(mock_httpx_client: Mock) -> None:
     request = Request(method=_METHOD, url=_URL)
-    retries = 3
+    retries = len(RETRY_ERROR_CODES)
+
     mock_httpx_client.request.side_effect = [
-        httpx.Response(status_code=429, request=request),
-        httpx.Response(status_code=429, request=request),
-        httpx.Response(status_code=429, request=request),
+        httpx.Response(status_code=status_code, request=request) for status_code in RETRY_ERROR_CODES
     ]
 
     client = HttpClient(client=mock_httpx_client, num_retries=retries)
-    with pytest.raises(TooManyRequestsError):
+    with pytest.raises(ServiceUnavailable):
         client.execute_http_request(method=_METHOD, url=_URL)
 
     assert mock_httpx_client.request.call_count == retries
