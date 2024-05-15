663a7c3c360955ff646f360e2ab35a9904098087
Josephasafg <ajgard7@gmail.com>
2024-05-15 16:01:37 +0300

    fix: tests

--- tests/unittests/clients/studio/resources/conftest.py
+++ tests/unittests/clients/studio/resources/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+import httpx
 from pytest_mock import MockerFixture
 
 from ai21.ai21_http_client import AI21HTTPClient
@@ -18,39 +19,24 @@ from ai21.models import (
     ChatMessage,
     RoleType,
     ChatResponse,
-    ChatOutput,
-    FinishReason,
-    Prompt,
-    Completion,
-    CompletionData,
-    CompletionFinishReason,
     CompletionsResponse,
     EmbedType,
     EmbedResponse,
-    EmbedResult,
     GECResponse,
-    Correction,
-    CorrectionType,
     ImprovementType,
     ImprovementsResponse,
-    Improvement,
     ParaphraseStyleType,
     ParaphraseResponse,
-    Suggestion,
     DocumentType,
     SegmentationResponse,
     SummaryMethod,
     SummarizeResponse,
     SummarizeBySegmentResponse,
-    SegmentSummary,
 )
 from ai21.models.chat import (
     ChatMessage as ChatCompletionChatMessage,
     ChatCompletionResponse,
-    ChatCompletionResponseChoice,
 )
-from ai21.models.responses.segmentation_response import Segment
-from ai21.models.usage_info import UsageInfo
 from ai21.utils.typing import to_lower_camel_case
 
 
@@ -59,9 +45,18 @@ def mock_ai21_studio_client(mocker: MockerFixture) -> AI21HTTPClient:
     return mocker.MagicMock(spec=AI21HTTPClient)
 
 
+@pytest.fixture
+def mock_successful_httpx_response(mocker: MockerFixture) -> httpx.Response:
+    mock_httpx_response = mocker.Mock(spec=httpx.Response)
+    mock_httpx_response.status_code = 200
+
+    return mock_httpx_response
+
+
 def get_studio_answer():
     _DUMMY_CONTEXT = "What is the answer to life, the universe and everything?"
     _DUMMY_QUESTION = "What is the answer?"
+    json_response = {"id": "some-id", "answer_in_context": True, "answer": "42"}
 
     return (
         StudioAnswer,
@@ -71,7 +66,8 @@ def get_studio_answer():
             "context": _DUMMY_CONTEXT,
             "question": _DUMMY_QUESTION,
         },
-        AnswerResponse(id="some-id", answer_in_context=True, answer="42"),
+        httpx.Response(status_code=200, json=json_response),
+        AnswerResponse.from_dict(json_response),
     )
 
 
@@ -85,6 +81,15 @@ def get_studio_chat():
         ),
     ]
     _DUMMY_SYSTEM = "You're a support engineer in a SaaS company"
+    json_response = {
+        "outputs": [
+            {
+                "text": "Hello, I need help with a signup process.",
+                "role": "user",
+                "finishReason": {"reason": "dummy_reason", "length": 1, "sequence": "1"},
+            }
+        ]
+    }
 
     return (
         StudioChat,
@@ -105,15 +110,8 @@ def get_studio_chat():
             "presencePenalty": None,
             "countPenalty": None,
         },
-        ChatResponse(
-            outputs=[
-                ChatOutput(
-                    text="Hello, I need help with a signup process.",
-                    role="user",
-                    finish_reason=FinishReason(reason="dummy_reason", length=1, sequence="1"),
-                )
-            ]
-        ),
+        httpx.Response(status_code=200, json=json_response),
+        ChatResponse.from_dict(json_response),
     )
 
 
@@ -127,6 +125,25 @@ def get_chat_completions():
         ),
     ]
     _EXPECTED_SERIALIZED_MESSAGES = [message.to_dict() for message in _DUMMY_MESSAGES]
+    json_response = {
+        "id": "some-id",
+        "choices": [
+            {
+                "index": 0,
+                "message": {
+                    "content": "Hello, I need help with a signup process.",
+                    "role": "user",
+                },
+                "finishReason": "dummy_reason",
+                "logprobs": None,
+            }
+        ],
+        "usage": {
+            "promptTokens": 10,
+            "completionTokens": 20,
+            "totalTokens": 30,
+        },
+    }
 
     return (
         ChatCompletions,
@@ -135,31 +152,26 @@ def get_chat_completions():
         {
             "model": _DUMMY_MODEL,
             "messages": _EXPECTED_SERIALIZED_MESSAGES,
+            "stream": False,
         },
-        ChatCompletionResponse(
-            id="some-id",
-            choices=[
-                ChatCompletionResponseChoice(
-                    index=0,
-                    message=ChatCompletionChatMessage(
-                        content="Hello, I need help with a signup process.", role=RoleType.USER
-                    ),
-                    finish_reason="dummy_reason",
-                    logprobs=None,
-                )
-            ],
-            usage=UsageInfo(
-                prompt_tokens=10,
-                completion_tokens=20,
-                total_tokens=30,
-            ),
-        ),
+        httpx.Response(status_code=200, json=json_response),
+        ChatCompletionResponse.from_dict(json_response),
     )
 
 
 def get_studio_completion(**kwargs):
     _DUMMY_MODEL = "dummy-completion-model"
     _DUMMY_PROMPT = "dummy-prompt"
+    json_response = {
+        "id": "some-id",
+        "completions": [
+            {
+                "data": {"text": "dummy-completion", "tokens": []},
+                "finishReason": {"reason": "dummy_reason", "length": 1},
+            }
+        ],
+        "prompt": {"text": "dummy-prompt"},
+    }
 
     return (
         StudioCompletion,
@@ -170,20 +182,19 @@ def get_studio_completion(**kwargs):
             "prompt": _DUMMY_PROMPT,
             **{to_lower_camel_case(k): v for k, v in kwargs.items()},
         },
-        CompletionsResponse(
-            id="some-id",
-            completions=[
-                Completion(
-                    data=CompletionData(text="dummy-completion", tokens=[]),
-                    finish_reason=CompletionFinishReason(reason="dummy_reason", length=1),
-                )
-            ],
-            prompt=Prompt(text="dummy-prompt"),
-        ),
+        httpx.Response(status_code=200, json=json_response),
+        CompletionsResponse.from_dict(json_response),
     )
 
 
 def get_studio_embed():
+    json_response = {
+        "id": "some-id",
+        "results": [
+            {"embedding": [1.0, 2.0, 3.0]},
+            {"embedding": [4.0, 5.0, 6.0]},
+        ],
+    }
     return (
         StudioEmbed,
         {"texts": ["text0", "text1"], "type": EmbedType.QUERY},
@@ -192,17 +203,24 @@ def get_studio_embed():
             "texts": ["text0", "text1"],
             "type": EmbedType.QUERY.value,
         },
-        EmbedResponse(
-            id="some-id",
-            results=[
-                EmbedResult([1.0, 2.0, 3.0]),
-                EmbedResult([4.0, 5.0, 6.0]),
-            ],
-        ),
+        httpx.Response(status_code=200, json=json_response),
+        EmbedResponse.from_dict(json_response),
     )
 
 
 def get_studio_gec():
+    json_response = {
+        "id": "some-id",
+        "corrections": [
+            {
+                "suggestion": "text to fix",
+                "startIndex": 9,
+                "endIndex": 10,
+                "originalText": "text to fi",
+                "correctionType": "Spelling",
+            }
+        ],
+    }
     text = "text to fi"
     return (
         StudioGEC,
@@ -211,22 +229,24 @@ def get_studio_gec():
         {
             "text": text,
         },
-        GECResponse(
-            id="some-id",
-            corrections=[
-                Correction(
-                    suggestion="text to fix",
-                    start_index=9,
-                    end_index=10,
-                    original_text=text,
-                    correction_type=CorrectionType.SPELLING,
-                )
-            ],
-        ),
+        httpx.Response(status_code=200, json=json_response),
+        GECResponse.from_dict(json_response),
     )
 
 
 def get_studio_improvements():
+    json_response = {
+        "id": "some-id",
+        "improvements": [
+            {
+                "suggestions": ["This text is improved"],
+                "startIndex": 0,
+                "endIndex": 15,
+                "originalText": "text to improve",
+                "improvementType": "FLUENCY",
+            }
+        ],
+    }
     text = "text to improve"
     types = [ImprovementType.FLUENCY]
     return (
@@ -237,18 +257,8 @@ def get_studio_improvements():
             "text": text,
             "types": types,
         },
-        ImprovementsResponse(
-            id="some-id",
-            improvements=[
-                Improvement(
-                    suggestions=["This text is improved"],
-                    start_index=0,
-                    end_index=15,
-                    original_text=text,
-                    improvement_type=ImprovementType.FLUENCY,
-                )
-            ],
-        ),
+        httpx.Response(status_code=200, json=json_response),
+        ImprovementsResponse.from_dict(json_response),
     )
 
 
@@ -257,6 +267,14 @@ def get_studio_paraphrase():
     style = ParaphraseStyleType.CASUAL
     start_index = 0
     end_index = 10
+    json_response = {
+        "id": "some-id",
+        "suggestions": [
+            {
+                "text": "This text is paraphrased",
+            }
+        ],
+    }
     return (
         StudioParaphrase,
         {"text": text, "style": style, "start_index": start_index, "end_index": end_index},
@@ -267,13 +285,24 @@ def get_studio_paraphrase():
             "startIndex": start_index,
             "endIndex": end_index,
         },
-        ParaphraseResponse(id="some-id", suggestions=[Suggestion(text="This text is paraphrased")]),
+        httpx.Response(status_code=200, json=json_response),
+        ParaphraseResponse.from_dict(json_response),
     )
 
 
 def get_studio_segmentation():
     source = "segmentation text"
     source_type = DocumentType.TEXT
+    json_response = {
+        "id": "some-id",
+        "segments": [
+            {
+                "segmentText": "This text is segmented",
+                "segmentType": "segment_type",
+            }
+        ],
+    }
+
     return (
         StudioSegmentation,
         {"source": source, "source_type": source_type},
@@ -282,9 +311,8 @@ def get_studio_segmentation():
             "source": source,
             "sourceType": source_type,
         },
-        SegmentationResponse(
-            id="some-id", segments=[Segment(segment_text="This text is segmented", segment_type="segment_type")]
-        ),
+        httpx.Response(status_code=200, json=json_response),
+        SegmentationResponse.from_dict(json_response),
     )
 
 
@@ -293,6 +321,11 @@ def get_studio_summarization():
     source_type = "TEXT"
     focus = "text"
     summary_method = SummaryMethod.FULL_DOCUMENT
+    json_response = {
+        "id": "some-id",
+        "summary": "This text is summarized",
+    }
+
     return (
         StudioSummarize,
         {"source": source, "source_type": source_type, "focus": focus, "summary_method": summary_method},
@@ -303,10 +336,8 @@ def get_studio_summarization():
             "focus": focus,
             "summaryMethod": summary_method,
         },
-        SummarizeResponse(
-            id="some-id",
-            summary="This text is summarized",
-        ),
+        httpx.Response(status_code=200, json=json_response),
+        SummarizeResponse.from_dict(json_response),
     )
 
 
@@ -314,6 +345,20 @@ def get_studio_summarize_by_segment():
     source = "text to summarize"
     source_type = "TEXT"
     focus = "text"
+    json_response = {
+        "id": "some-id",
+        "segments": [
+            {
+                "summary": "This text is summarized",
+                "segmentText": "This text is segmented",
+                "segmentHtml": "",
+                "segmentType": "segment_type",
+                "hasSummary": True,
+                "highlights": [],
+            }
+        ],
+    }
+
     return (
         StudioSummarizeBySegment,
         {"source": source, "source_type": source_type, "focus": focus},
@@ -323,17 +368,6 @@ def get_studio_summarize_by_segment():
             "sourceType": source_type,
             "focus": focus,
         },
-        SummarizeBySegmentResponse(
-            id="some-id",
-            segments=[
-                SegmentSummary(
-                    summary="This text is summarized",
-                    segment_text="This text is segmented",
-                    segment_type="segment_type",
-                    segment_html=None,
-                    has_summary=True,
-                    highlights=[],
-                )
-            ],
-        ),
+        httpx.Response(status_code=200, json=json_response),
+        SummarizeBySegmentResponse.from_dict(json_response),
     )
