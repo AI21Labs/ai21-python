2f2b7cb122d3b95640db7fe1e255e1ae3a1aa9b9
Josephasafg <ajgard7@gmail.com>
2024-05-15 16:25:21 +0300

    fix: sagemaker tests

--- tests/unittests/conftest.py
+++ tests/unittests/conftest.py
@@ -10,3 +10,8 @@ def dummy_api_host() -> str:
 @pytest.fixture
 def mock_httpx_client(mocker) -> httpx.Client:
     return mocker.Mock(spec=httpx.Client)
+
+
+@pytest.fixture
+def mock_httpx_response(mocker) -> httpx.Response:
+    return mocker.Mock(spec=httpx.Response)
--- tests/unittests/services/test_sagemaker.py
+++ tests/unittests/services/test_sagemaker.py
@@ -1,3 +1,4 @@
+import httpx
 import pytest
 
 from ai21 import ModelPackageDoesntExistError
@@ -8,28 +9,29 @@ _DUMMY_VERSIONS = ["1.0.0", "1.0.1"]
 
 
 class TestSageMakerService:
-    def test__get_model_package_arn__should_return_model_package_arn(self):
-        expected_response = {
+    def test__get_model_package_arn__should_return_model_package_arn(self, mock_httpx_response: httpx.Response):
+        mock_httpx_response.json.return_value = {
             "arn": _DUMMY_ARN,
             "versions": _DUMMY_VERSIONS,
         }
-        SageMakerStub.ai21_http_client.execute_http_request.return_value = expected_response
+        SageMakerStub.ai21_http_client.execute_http_request.return_value = mock_httpx_response
 
         actual_model_package_arn = SageMakerStub.get_model_package_arn(model_name="j2-mid", region="us-east-1")
 
         assert actual_model_package_arn == _DUMMY_ARN
 
-    def test__get_model_package_arn__when_no_arn__should_raise_error(self):
-        SageMakerStub.ai21_http_client.execute_http_request.return_value = {"arn": []}
+    def test__get_model_package_arn__when_no_arn__should_raise_error(self, mock_httpx_response: httpx.Response):
+        mock_httpx_response.json.return_value = {"arn": []}
+        SageMakerStub.ai21_http_client.execute_http_request.return_value = mock_httpx_response
 
         with pytest.raises(ModelPackageDoesntExistError):
             SageMakerStub.get_model_package_arn(model_name="j2-mid", region="us-east-1")
 
-    def test__list_model_package_versions__should_return_model_package_arn(self):
-        expected_response = {
+    def test__list_model_package_versions__should_return_model_package_arn(self, mock_httpx_response: httpx.Response):
+        mock_httpx_response.json.return_value = {
             "versions": _DUMMY_VERSIONS,
         }
-        SageMakerStub.ai21_http_client.execute_http_request.return_value = expected_response
+        SageMakerStub.ai21_http_client.execute_http_request.return_value = mock_httpx_response
 
         actual_model_package_arn = SageMakerStub.list_model_package_versions(model_name="j2-mid", region="us-east-1")
 
