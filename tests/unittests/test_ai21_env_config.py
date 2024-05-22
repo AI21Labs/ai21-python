87a17c4a6b54c76efdaef6e84f084d780a78117b
Josephasafg <ajgard7@gmail.com>
2024-05-22 18:24:36 +0300

    fix: test order

--- tests/unittests/test_ai21_env_config.py
+++ tests/unittests/test_ai21_env_config.py
@@ -5,6 +5,16 @@ _FAKE_API_KEY = "fake-key"
 os.environ["AI21_API_KEY"] = _FAKE_API_KEY
 
 
+def test_env_config__when_set_via_init_and_env__should_be_taken_from_init():
+    client = AI21Client()
+    assert client._http_client._api_key == _FAKE_API_KEY
+
+    init_api_key = "init-key"
+    client2 = AI21Client(api_key=init_api_key)
+
+    assert client2._http_client._api_key == init_api_key
+
+
 def test_env_config__when_set_twice__should_be_updated():
     client = AI21Client()
 
@@ -14,16 +24,6 @@ def test_env_config__when_set_twice__should_be_updated():
     os.environ["AI21_API_KEY"] = new_api_key
     client2 = AI21Client()
     assert client2._http_client._api_key == new_api_key
-
-
-def test_env_config__when_set_via_init_and_env__should_be_taken_from_init():
-    client = AI21Client()
-    assert client._http_client._api_key == _FAKE_API_KEY
-
-    init_api_key = "init-key"
-    client2 = AI21Client(api_key=init_api_key)
-
-    assert client2._http_client._api_key == init_api_key
 
 
 def test_env_config__when_set_int__should_be_set():
