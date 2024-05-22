757d5414e88daa33095714138e07344f053f6654
Josephasafg <ajgard7@gmail.com>
2024-05-22 18:27:00 +0300

    fix: Added contextmanager set_env_var

--- tests/unittests/test_ai21_env_config.py
+++ tests/unittests/test_ai21_env_config.py
@@ -1,8 +1,17 @@
 import os
+from contextlib import contextmanager
+
 from ai21 import AI21Client
 
 _FAKE_API_KEY = "fake-key"
 os.environ["AI21_API_KEY"] = _FAKE_API_KEY
+
+
+@contextmanager
+def set_env_var(key: str, value: str):
+    os.environ[key] = value
+    yield
+    del os.environ[key]
 
 
 def test_env_config__when_set_via_init_and_env__should_be_taken_from_init():
@@ -21,14 +30,14 @@ def test_env_config__when_set_twice__should_be_updated():
     assert client._http_client._api_key == _FAKE_API_KEY
 
     new_api_key = "new-key"
-    os.environ["AI21_API_KEY"] = new_api_key
-    client2 = AI21Client()
-    assert client2._http_client._api_key == new_api_key
+
+    with set_env_var("AI21_API_KEY", new_api_key):
+        client2 = AI21Client()
+        assert client2._http_client._api_key == new_api_key
 
 
 def test_env_config__when_set_int__should_be_set():
-    os.environ["AI21_TIMEOUT_SEC"] = "1"
+    with set_env_var("AI21_TIMEOUT_SEC", "1"):
+        client = AI21Client()
 
-    client = AI21Client()
-
-    assert client._http_client._timeout_sec == 1
+        assert client._http_client._timeout_sec == 1
