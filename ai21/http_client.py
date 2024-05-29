04d88842ced6ec9014716c2ec6029c9761251ecd
Miri Bar <mirib@ai21.com>
2024-05-29 18:40:15 +0300

    feat: async support - stream, http, ai21 http

--- ai21/http_client.py
+++ ai21/http_client.py
@@ -1,5 +1,5 @@
 import json
-from typing import Optional, Dict, Any, BinaryIO
+from typing import Optional, Dict, Any, BinaryIO, TypeVar, Union, Generic
 
 import httpx
 from httpx import ConnectError
@@ -15,6 +15,11 @@ from ai21.errors import (
     AI21APIError,
 )
 from ai21.logger import logger
+from ai21.stream import Stream, AsyncStream
+
+_HttpxClientT = TypeVar("_HttpxClientT", bound=Union[httpx.Client, httpx.AsyncClient])
+_DefaultStreamT = TypeVar("_DefaultStreamT", bound=Union[Stream[Any], AsyncStream[Any]])
+
 
 DEFAULT_TIMEOUT_SEC = 300
 DEFAULT_NUM_RETRIES = 0
@@ -46,10 +51,18 @@ def _requests_retry_session(retries: int) -> httpx.HTTPTransport:
     )
 
 
-class HttpClient:
+def _requests_retry_async_session(retries: int) -> httpx.AsyncHTTPTransport:
+    return httpx.AsyncHTTPTransport(
+        retries=retries,
+    )
+
+
+class BaseHttpClient(Generic[_HttpxClientT, _DefaultStreamT]):
+    _client: Optional[_HttpxClientT] = None
+    _request: Any
+
     def __init__(
         self,
-        client: Optional[httpx.Client] = None,
         timeout_sec: int = None,
         num_retries: int = None,
         headers: Dict = None,
@@ -58,6 +71,66 @@ class HttpClient:
         self._num_retries = num_retries or DEFAULT_NUM_RETRIES
         self._headers = headers or {}
         self._apply_retry_policy = self._num_retries > 0
+
+    def _should_retry(self, response: httpx.Response) -> bool:
+        return response.status_code in RETRY_ERROR_CODES and response.request.method in RETRY_METHOD_WHITELIST
+
+    def _get_data(
+        self,
+        files: Optional[Dict[str, BinaryIO]],
+        method: str,
+        params: Optional[Dict],
+    ) -> Dict[str, Any] | bytes:
+        headers = self._headers
+        if files is not None:
+            if method != "POST":
+                raise ValueError(
+                    f"execute_http_request supports only POST for files upload, but {method} was supplied instead"
+                )
+            if "Content-Type" in headers:
+                headers.pop(
+                    "Content-Type"
+                )  # multipart/form-data 'Content-Type' is being added when passing rb files and payload
+            return params
+        else:
+            return json.dumps(params).encode() if params else None
+
+    def add_headers(self, headers: Dict[str, Any]) -> None:
+        self._headers.update(headers)
+
+    def execute_http_request(
+        self,
+        method: str,
+        url: str,
+        params: Optional[Dict] = None,
+        stream: bool = False,
+        files: Optional[Dict[str, BinaryIO]] = None,
+    ) -> httpx.Response:
+        raise NotImplementedError("Subclasses must implement this method")
+
+    def _request(
+        self,
+        files: Optional[Dict[str, BinaryIO]],
+        method: str,
+        params: Optional[Dict],
+        url: str,
+        stream: bool,
+    ) -> httpx.Response:
+        raise NotImplementedError("Subclasses must implement this method")
+
+    def _init_client(self, client: Optional[_HttpxClientT]) -> _HttpxClientT:
+        raise NotImplementedError("Subclasses must implement this method")
+
+
+class HttpClient(BaseHttpClient[httpx.Client, Stream[Any]]):
+    def __init__(
+        self,
+        client: Optional[httpx.Client] = None,
+        timeout_sec: int = None,
+        num_retries: int = None,
+        headers: Dict = None,
+    ):
+        super().__init__(timeout_sec=timeout_sec, num_retries=num_retries, headers=headers)
         self._client = self._init_client(client)
 
         # Since we can't use the retry decorator on a method of a class as we can't access class attributes,
@@ -67,9 +140,6 @@ class HttpClient:
             retry=retry_if_result(self._should_retry),
             stop=stop_after_attempt(self._num_retries),
         )(self._request)
-
-    def _should_retry(self, response: httpx.Response) -> bool:
-        return response.status_code in RETRY_ERROR_CODES and response.request.method in RETRY_METHOD_WHITELIST
 
     def execute_http_request(
         self,
@@ -116,23 +186,16 @@ class HttpClient:
 
         if method == "GET":
             request = self._client.build_request(
-                method=method, url=url, headers=headers, timeout=timeout, params=params
+                method=method,
+                url=url,
+                headers=headers,
+                timeout=timeout,
+                params=params,
             )
 
             return self._client.send(request=request, stream=stream)
 
-        if files is not None:
-            if method != "POST":
-                raise ValueError(
-                    f"execute_http_request supports only POST for files upload, but {method} was supplied instead"
-                )
-            if "Content-Type" in headers:
-                headers.pop(
-                    "Content-Type"
-                )  # multipart/form-data 'Content-Type' is being added when passing rb files and payload
-            data = params
-        else:
-            data = json.dumps(params).encode() if params else None
+        data = self._get_data(files=files, method=method, params=params)
 
         request = self._client.build_request(
             method=method,
@@ -154,5 +217,105 @@ class HttpClient:
 
         return httpx.Client()
 
-    def add_headers(self, headers: Dict[str, Any]) -> None:
-        self._headers.update(headers)
+
+class AsyncHttpClient(BaseHttpClient[httpx.AsyncClient, AsyncStream[Any]]):
+    def __init__(
+        self,
+        client: Optional[httpx.AsyncClient] = None,
+        timeout_sec: int = None,
+        num_retries: int = None,
+        headers: Dict = None,
+    ):
+        super().__init__(timeout_sec=timeout_sec, num_retries=num_retries, headers=headers)
+        self._client = self._init_client(client)
+
+        # Since we can't use the retry decorator on a method of a class as we can't access class attributes,
+        # we have to wrap the method in a function
+        self._request = retry(
+            wait=wait_exponential(multiplier=RETRY_BACK_OFF_FACTOR, min=TIME_BETWEEN_RETRIES),
+            retry=retry_if_result(self._should_retry),
+            stop=stop_after_attempt(self._num_retries),
+        )(self._request)
+
+    async def execute_http_request(
+        self,
+        method: str,
+        url: str,
+        params: Optional[Dict] = None,
+        stream: bool = False,
+        files: Optional[Dict[str, BinaryIO]] = None,
+    ) -> httpx.Response:
+        try:
+            response = await self._request(
+                files=files,
+                method=method,
+                params=params,
+                url=url,
+                stream=stream,
+            )
+        except RetryError as retry_error:
+            last_attempt = retry_error.last_attempt
+
+            if last_attempt.failed:
+                raise last_attempt.exception()
+            else:
+                response = last_attempt.result()
+
+        except ConnectError as connection_error:
+            logger.error(f"Calling {method} {url} failed with ConnectionError: {connection_error}")
+            raise connection_error
+        except Exception as exception:
+            logger.error(f"Calling {method} {url} failed with Exception: {exception}")
+            raise exception
+
+        if response.status_code != httpx.codes.OK:
+            logger.error(f"Calling {method} {url} failed with a non-200 response code: {response.status_code}")
+            handle_non_success_response(response.status_code, response.text)
+
+        return response
+
+    async def _request(
+        self,
+        files: Optional[Dict[str, BinaryIO]],
+        method: str,
+        params: Optional[Dict],
+        url: str,
+        stream: bool,
+    ) -> httpx.Response:
+        timeout = self._timeout_sec
+        headers = self._headers
+        logger.debug(f"Calling {method} {url} {headers} {params}")
+
+        if method == "GET":
+            request = self._client.build_request(
+                method=method,
+                url=url,
+                headers=headers,
+                timeout=timeout,
+                params=params,
+            )
+
+            return await self._client.send(request=request, stream=stream)
+
+        data = self._get_data(files=files, method=method, params=params)
+
+        request = self._client.build_request(
+            method=method,
+            url=url,
+            headers=headers,
+            data=data,
+            timeout=timeout,
+            files=files,
+        )
+
+        return await self._client.send(request=request, stream=stream)
+
+    def _init_client(self, client: Optional[httpx.AsyncClient]) -> httpx.AsyncClient:
+        if client is not None:
+            return client
+
+        return (
+            _requests_retry_async_session(retries=self._num_retries)
+            if self._apply_retry_policy
+            else httpx.AsyncClient()
+        )
--- ai21/stream.py
+++ ai21/stream.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import json
 from types import TracebackType
-from typing import TypeVar, Generic, Iterator, Optional
+from typing import TypeVar, Generic, Iterator, Optional, AsyncIterator
 
 import httpx
 from typing_extensions import Self
@@ -64,9 +64,67 @@ class Stream(Generic[_T]):
         self.response.close()
 
 
+class AsyncStream(Generic[_T]):
+    response: httpx.Response
+
+    def __init__(
+        self,
+        *,
+        cast_to: type[_T],
+        response: httpx.Response,
+    ):
+        self.response = response
+        self.cast_to = cast_to
+        self._decoder = _SSEDecoder()
+        self._iterator = self.__stream__()
+
+    async def __anext__(self) -> _T:
+        return await self._iterator.__anext__()
+
+    async def __aiter__(self) -> AsyncIterator[_T]:
+        async for item in self._iterator:
+            yield item
+
+    async def __stream__(self) -> AsyncIterator[_T]:
+        async for chunk in self._decoder.aiter(self.response.aiter_lines()):
+            if chunk.endswith(_SSE_DONE_MSG):
+                break
+
+            try:
+                chunk = json.loads(chunk)
+                if hasattr(self.cast_to, "from_dict"):
+                    yield self.cast_to.from_dict(chunk)
+                else:
+                    yield self.cast_to(**chunk)
+            except json.JSONDecodeError:
+                raise StreamingDecodeError(chunk)
+
+    async def __aenter__(self) -> Self:
+        return self
+
+    async def __aexit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc: BaseException | None,
+        exc_tb: TracebackType | None,
+    ) -> None:
+        await self.close()
+
+    async def close(self):
+        await self.response.aclose()
+
+
 class _SSEDecoder:
     def iter(self, iterator: Iterator[str]):
         for line in iterator:
+            line = line.strip()
+            decoded_line = self._decode(line)
+
+            if decoded_line is not None:
+                yield decoded_line
+
+    async def aiter(self, iterator: AsyncIterator[str]):
+        async for line in iterator:
             line = line.strip()
             decoded_line = self._decode(line)
 
