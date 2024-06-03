4abae9d29ea1f0f0d8f51ea2b595fa647eb21b11
Miri Bar <mirib@ai21.com>
2024-06-03 19:53:53 +0300

    feat: embed, gec, improvements

--- ai21/clients/studio/resources/studio_gec.py
+++ ai21/clients/studio/resources/studio_gec.py
@@ -1,5 +1,5 @@
 from ai21.clients.common.gec_base import GEC
-from ai21.clients.studio.resources.studio_resource import StudioResource
+from ai21.clients.studio.resources.studio_resource import StudioResource, AsyncStudioResource
 from ai21.models import GECResponse
 
 
@@ -9,3 +9,11 @@ class StudioGEC(StudioResource, GEC):
         url = f"{self._client.get_base_url()}/{self._module_name}"
 
         return self._post(url=url, body=body, response_cls=GECResponse)
+
+
+class AsyncStudioGEC(AsyncStudioResource, GEC):
+    async def create(self, text: str, **kwargs) -> GECResponse:
+        body = self._create_body(text=text, **kwargs)
+        url = f"{self._client.get_base_url()}/{self._module_name}"
+
+        return await self._post(url=url, body=body, response_cls=GECResponse)
--- ai21/clients/studio/resources/studio_improvements.py
+++ ai21/clients/studio/resources/studio_improvements.py
@@ -1,7 +1,7 @@
 from typing import List
 
 from ai21.clients.common.improvements_base import Improvements
-from ai21.clients.studio.resources.studio_resource import StudioResource
+from ai21.clients.studio.resources.studio_resource import StudioResource, AsyncStudioResource
 from ai21.errors import EmptyMandatoryListError
 from ai21.models import ImprovementType, ImprovementsResponse
 
@@ -15,3 +15,14 @@ class StudioImprovements(StudioResource, Improvements):
         body = self._create_body(text=text, types=types, **kwargs)
 
         return self._post(url=url, body=body, response_cls=ImprovementsResponse)
+
+
+class AsyncStudioImprovements(AsyncStudioResource, Improvements):
+    async def create(self, text: str, types: List[ImprovementType], **kwargs) -> ImprovementsResponse:
+        if len(types) == 0:
+            raise EmptyMandatoryListError("types")
+
+        url = f"{self._client.get_base_url()}/{self._module_name}"
+        body = self._create_body(text=text, types=types, **kwargs)
+
+        return await self._post(url=url, body=body, response_cls=ImprovementsResponse)
