aa767630db66fed9c717096f14787fbc32b25d7c
Miri Bar <mirib@ai21.com>
2024-06-06 10:55:18 +0300

    fix: tests

--- tests/integration_tests/clients/studio/test_chat_completions.py
+++ tests/integration_tests/clients/studio/test_chat_completions.py
@@ -1,9 +1,10 @@
 import json
 from unittest.mock import patch
+import pytest
 
 import httpx
 
-from ai21 import AI21Client
+from ai21 import AI21Client, AsyncAI21Client
 from ai21.models.chat import ChatMessage, ChatCompletionResponse, ChatCompletionChunk, ChoicesChunk, ChoiceDelta
 from ai21.models import RoleType
 
@@ -106,3 +107,89 @@ def test_chat_completion__when_stream__should_return_chunks():
         assert isinstance(chunk, ChatCompletionChunk)
         assert isinstance(chunk.choices[0], ChoicesChunk)
         assert isinstance(chunk.choices[0].delta, ChoiceDelta)
+
+
+@pytest.mark.asyncio
+async def test_async_chat_completion():
+    messages = _MESSAGES
+
+    client = AsyncAI21Client()
+    response = await client.chat.completions.create(
+        model=_MODEL,
+        messages=messages,
+        max_tokens=64,
+        temperature=0.7,
+        stop=["\n"],
+        top_p=0.3,
+    )
+
+    assert isinstance(response, ChatCompletionResponse)
+    assert response.choices[0].message.content
+    assert response.choices[0].message.role
+
+
+@pytest.mark.asyncio
+async def test_async_chat_completion__when_stream__should_return_chunks():
+    messages = _MESSAGES
+
+    client = AsyncAI21Client()
+
+    response = await client.chat.completions.create(
+        model=_MODEL,
+        messages=messages,
+        temperature=0,
+        stream=True,
+    )
+
+    async for chunk in response:
+        assert isinstance(chunk, ChatCompletionChunk)
+        assert isinstance(chunk.choices[0], ChoicesChunk)
+        assert isinstance(chunk.choices[0].delta, ChoiceDelta)
+
+
+@pytest.mark.asyncio
+async def test_async_chat_completion_when_num_retries_is_over_1__should_retry():
+    num_retries = 3
+
+    with patch.object(
+        httpx.Client,
+        "send",
+        side_effect=[
+            _BAD_HTTPX_RESPONSE,
+            _BAD_HTTPX_RESPONSE,
+            httpx.Response(status_code=200, content=json.dumps(_EXPECTED_MESSAGE_CONTENT)),
+        ],
+    ) as mock_send:
+        messages = _MESSAGES
+
+        client = AsyncAI21Client(num_retries=num_retries)
+        response = await client.chat.completions.create(
+            model=_MODEL,
+            messages=messages,
+        )
+
+        assert isinstance(response, ChatCompletionResponse)
+        assert mock_send.call_count == num_retries
+
+
+@pytest.mark.asyncio
+async def test_async_chat_completion__with_n_param__should_return_n_choices():
+    messages = _MESSAGES
+    n = 3
+
+    client = AsyncAI21Client()
+    response = await client.chat.completions.create(
+        model=_MODEL,
+        messages=messages,
+        max_tokens=64,
+        temperature=0.7,
+        stop=["\n"],
+        top_p=0.3,
+        n=n,
+    )
+
+    assert isinstance(response, ChatCompletionResponse)
+    assert len(response.choices) == n
+    for choice in response.choices:
+        assert choice.message.content
+        assert choice.message.role
