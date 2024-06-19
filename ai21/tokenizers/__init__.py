45cb3eef6f7329aa55b2cc6cd4513e97ac95a3c4
miri-bar <160584887+miri-bar@users.noreply.github.com>
2024-06-19 13:41:10 +0300

    feat: Add async tokenizer, add detokenize method (#144)
    * feat: add detokenize method, add async tokenizer
    
    * chore: update pyproject and poetry.lock
    
    * fix: fix tokenizer name in examples and readme, add example

--- ai21/tokenizers/__init__.py
+++ ai21/tokenizers/__init__.py
@@ -1,4 +1,4 @@
 from .ai21_tokenizer import AI21Tokenizer
-from .factory import get_tokenizer
+from .factory import get_tokenizer, get_async_tokenizer
 
-__all__ = ["AI21Tokenizer", "get_tokenizer"]
+__all__ = ["AI21Tokenizer", "get_tokenizer", "get_async_tokenizer"]
--- ai21/tokenizers/factory.py
+++ ai21/tokenizers/factory.py
@@ -2,9 +2,10 @@ from typing import Dict
 
 from ai21_tokenizer import Tokenizer, PreTrainedTokenizers
 
-from ai21.tokenizers.ai21_tokenizer import AI21Tokenizer
+from ai21.tokenizers.ai21_tokenizer import AI21Tokenizer, AsyncAI21Tokenizer
 
 _cached_tokenizers: Dict[str, AI21Tokenizer] = {}
+_cached_async_tokenizers: Dict[str, AsyncAI21Tokenizer] = {}
 
 
 def get_tokenizer(name: str = PreTrainedTokenizers.J2_TOKENIZER) -> AI21Tokenizer:
@@ -19,3 +20,17 @@ def get_tokenizer(name: str = PreTrainedTokenizers.J2_TOKENIZER) -> AI21Tokenize
         _cached_tokenizers[name] = AI21Tokenizer(Tokenizer.get_tokenizer(name))
 
     return _cached_tokenizers[name]
+
+
+async def get_async_tokenizer(name: str = PreTrainedTokenizers.J2_TOKENIZER) -> AsyncAI21Tokenizer:
+    """
+    Get the async tokenizer instance.
+
+    If the tokenizer instance is not cached, it will be created using the Tokenizer.get_tokenizer() method.
+    """
+    global _cached_async_tokenizers
+
+    if _cached_async_tokenizers.get(name) is None:
+        _cached_async_tokenizers[name] = AsyncAI21Tokenizer(await Tokenizer.get_async_tokenizer(name))
+
+    return _cached_async_tokenizers[name]
