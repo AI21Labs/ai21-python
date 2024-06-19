caf97f5138e53f7ae8f8c118b67721bffd3254f9
Miri Bar <mirib@ai21.com>
2024-06-19 14:54:12 +0300

    test: adjust unittest + add unittest for async

--- tests/unittests/clients/bedrock/resources/conftest.py
+++ tests/unittests/clients/bedrock/resources/conftest.py
@@ -3,9 +3,14 @@ from unittest.mock import Mock
 import pytest
 from pytest_mock import MockerFixture
 
-from ai21.clients.bedrock.bedrock_session import BedrockSession
+from ai21.clients.aws_http_client.aws_http_client import AWSHttpClient, AsyncAWSHttpClient
 
 
 @pytest.fixture
-def mock_bedrock_session(mocker: MockerFixture) -> Mock:
-    return mocker.MagicMock(spec=BedrockSession)
+def mock_aws_http_client(mocker: MockerFixture) -> Mock:
+    return mocker.MagicMock(spec=AWSHttpClient)
+
+
+@pytest.fixture
+def mock_async_aws_http_client(mocker: MockerFixture) -> Mock:
+    return mocker.MagicMock(spec=AsyncAWSHttpClient)
