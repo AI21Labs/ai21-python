6156deebbda39b9bd0031e4289740d58a9e7e199
Miri Bar <mirib@ai21.com>
2024-06-27 11:38:13 +0300

    refactor: remove aws_http_client, use http_client instead, add aws auth test

--- ai21/clients/bedrock/ai21_bedrock_client.py
+++ ai21/clients/bedrock/ai21_bedrock_client.py
@@ -3,10 +3,12 @@ from typing import Optional, Dict, Any
 import boto3
 
 from ai21.ai21_env_config import _AI21EnvConfig, AI21EnvConfig
-from ai21.clients.aws_http_client.aws_http_client import AWSHttpClient, AsyncAWSHttpClient, DEFAULT_AWS_REGION
+
 from ai21.clients.bedrock.resources.bedrock_completion import BedrockCompletion, AsyncBedrockCompletion
 from ai21.http_client.http_client import HttpClient
 from ai21.http_client.async_http_client import AsyncHttpClient
+
+DEFAULT_AWS_REGION = "us-east-1"
 
 
 class AI21BedrockClient:
@@ -24,16 +26,16 @@ class AI21BedrockClient:
         self._model_id = model_id
         self._bedrock_session = session
         region = session.region_name if session is not None else region or env_config.aws_region or DEFAULT_AWS_REGION
-        self._http_client = AWSHttpClient(
-            aws_region=region,
-            headers=headers,
+
+        self._http_client = http_client or HttpClient(
             timeout_sec=timeout_sec,
             num_retries=num_retries,
-            http_client=http_client,
-            aws_session=session,
+            headers=headers,
         )
 
-        self.completion = BedrockCompletion(model_id=model_id, region=region, client=self._http_client)
+        self.completion = BedrockCompletion(
+            model_id=model_id, region=region, client=self._http_client, aws_session=session
+        )
 
 
 class AsyncAI21BedrockClient:
@@ -50,13 +52,10 @@ class AsyncAI21BedrockClient:
         self._model_id = model_id
         self._bedrock_session = session
         region = session.region_name if session is not None else region or DEFAULT_AWS_REGION
-        self._http_client = AsyncAWSHttpClient(
-            aws_region=region,
-            headers=headers,
+        self._http_client = http_client or AsyncHttpClient(
             timeout_sec=timeout_sec,
             num_retries=num_retries,
-            http_client=http_client,
-            aws_session=session,
+            headers=headers,
         )
 
         self.completion = AsyncBedrockCompletion(model_id=model_id, region=region, client=self._http_client)
--- ai21/clients/bedrock/resources/bedrock_resource.py
+++ ai21/clients/bedrock/resources/bedrock_resource.py
@@ -1,23 +1,30 @@
 from __future__ import annotations
 
+import json
 from abc import ABC
 from typing import Any, Dict, Optional
 
+import boto3
 import httpx
 
-from ai21.clients.aws_http_client.aws_http_client import AWSHttpClient, AsyncAWSHttpClient
+from ai21.clients.aws_http_client.aws_authorization import AWSAuthorization
+from ai21.http_client.async_http_client import AsyncHttpClient
+from ai21.http_client.http_client import HttpClient
 
 
 class BedrockResource(ABC):
     def __init__(
         self,
         region: str,
-        client: AWSHttpClient,
+        client: HttpClient,
         model_id: Optional[str] = None,
+        aws_session: Optional[boto3.Session] = None,
     ):
         self._region = region
         self._model_id = model_id
         self._client = client
+        self._aws_session = aws_session or boto3.Session(region_name=region)
+        self._aws_auth = AWSAuthorization(aws_session=self._aws_session)
 
     def _post(
         self,
@@ -25,7 +32,11 @@ class BedrockResource(ABC):
         model_id: str,
     ) -> httpx.Response:
         url = self._build_url(model_id=model_id)
-        return self._client.execute_http_request(url=url, body=body, method="POST", service_name="bedrock")
+        auth_headers = self._aws_auth.prepare_auth_headers(
+            service_name="bedrock", url=url, method="POST", data=json.dumps(body)
+        )
+
+        return self._client.execute_http_request(url=url, body=body, method="POST", extra_headers=auth_headers)
 
     def _build_url(self, model_id: str) -> str:
         return f"https://bedrock-runtime.{self._region}.amazonaws.com/model/{model_id}/invoke"
@@ -35,12 +46,15 @@ class AsyncBedrockResource(ABC):
     def __init__(
         self,
         region: str,
-        client: AsyncAWSHttpClient,
+        client: AsyncHttpClient,
         model_id: Optional[str] = None,
+        aws_session: Optional[boto3.Session] = None,
     ):
         self._region = region
         self._model_id = model_id
         self._client = client
+        self._aws_session = aws_session or boto3.Session(region_name=region)
+        self._aws_auth = AWSAuthorization(aws_session=self._aws_session)
 
     async def _post(
         self,
@@ -48,7 +62,11 @@ class AsyncBedrockResource(ABC):
         model_id: str,
     ) -> httpx.Response:
         url = self._build_url(model_id=model_id)
-        return await self._client.execute_http_request(url=url, body=body, method="POST", service_name="bedrock")
+        auth_headers = self._aws_auth.prepare_auth_headers(
+            service_name="bedrock", url=url, method="POST", data=json.dumps(body)
+        )
+
+        return await self._client.execute_http_request(url=url, body=body, method="POST", extra_headers=auth_headers)
 
     def _build_url(self, model_id: str) -> str:
         return f"https://bedrock-runtime.{self._region}.amazonaws.com/model/{model_id}/invoke"
