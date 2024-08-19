c9e1b3a69c15edfe12659199b397643334c82c90
amirk <amirk@ai21.com>
2024-08-19 18:56:27 +0300

    feat: simplify models

--- ai21/models/chat/document_schema.py
+++ ai21/models/chat/document_schema.py
@@ -1,10 +1,8 @@
-from __future__ import annotations
 from typing import Optional
-from pydantic import Field
 from ai21.models.ai21_base_model import AI21BaseModel
 
 
 class DocumentSchema(AI21BaseModel):
-    id: Optional[str] = Field(None, description="Id of the document")
-    content: str = Field(description="Content of the document")
-    metadata: Optional[dict[str, str]] = Field({}, description="Metadata of the document")
+    id: Optional[str] = None
+    content: str
+    metadata: Optional[dict[str, str]] = None
