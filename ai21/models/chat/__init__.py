17d6628be210dae90f595f9cc2c27ff0ecca3ed8
Josephasafg <ajgard7@gmail.com>
2024-08-21 14:47:53 +0300

    fix: Added ChatMessageParam to __init__

--- ai21/models/chat/__init__.py
+++ ai21/models/chat/__init__.py
@@ -1,18 +1,17 @@
 from __future__ import annotations
 
+from .chat_completion_chunk import ChatCompletionChunk, ChoicesChunk, ChoiceDelta
 from .chat_completion_response import ChatCompletionResponse
 from .chat_completion_response import ChatCompletionResponseChoice
-from .chat_message import ChatMessage, AssistantMessage, ToolMessage, UserMessage, SystemMessage
+from .chat_message import ChatMessage, AssistantMessage, ToolMessage, UserMessage, SystemMessage, ChatMessageParam
 from .document_schema import DocumentSchema
 from .function_tool_definition import FunctionToolDefinition
 from .response_format import ResponseFormat
 from .role_type import RoleType as RoleType
-from .chat_completion_chunk import ChatCompletionChunk, ChoicesChunk, ChoiceDelta
 from .tool_call import ToolCall
 from .tool_defintions import ToolDefinition
 from .tool_function import ToolFunction
 from .tool_parameters import ToolParameters
-
 
 __all__ = [
     "ChatCompletionResponse",
@@ -26,6 +25,7 @@ __all__ = [
     "ToolMessage",
     "UserMessage",
     "SystemMessage",
+    "ChatMessageParam",
     "DocumentSchema",
     "FunctionToolDefinition",
     "ResponseFormat",
