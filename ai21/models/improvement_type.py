b39c92cfbdc88c9f98f3d5bfb20aa850f6211b78
amirai21 <89905406+amirai21@users.noreply.github.com>
2024-11-11 14:30:09 +0200

    feat: Sunset tsms and j2 (#225)
    feat!: Removed Legacy TSM Models

--- ai21/models/improvement_type.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class ImprovementType(str, Enum):
-    FLUENCY = "fluency"
-    VOCABULARY_SPECIFICITY = "vocabulary/specificity"
-    VOCABULARY_VARIETY = "vocabulary/variety"
-    CLARITY_SHORT_SENTENCES = "clarity/short-sentences"
-    CLARITY_CONCISENESS = "clarity/conciseness"
--- examples/studio/library_search.py
+++
@@ -1,5 +0,0 @@
-from ai21 import AI21Client
-
-client = AI21Client()
-response = client.library.search.create(query="cat colors", max_segments=2)
-print(response)
