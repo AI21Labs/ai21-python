4d2bb958414ec1af42484462d9564e7da2a177ea
benshuk <bens@ai21.com>
2024-12-03 12:00:02 +0200

    feat: :sparkles: add examples

---
+++ examples/studio/assistant/assistant.py
@@ -0,0 +1,38 @@
+from ai21 import AI21Client
+
+
+def main():
+    ai21_client = AI21Client()
+
+    assistant = ai21_client.beta.assistants.create(name="My Assistant")
+
+    thread = ai21_client.beta.threads.create(
+        messages=[
+            {
+                "role": "user",
+                "content": {
+                    "type": "text",
+                    "text": "Hello",
+                },
+            },
+        ]
+    )
+
+    run = ai21_client.beta.threads.runs.create(
+        thread_id=thread.id,
+        assistant_id=assistant.id,
+        description="Get response",
+        optimization="latency",
+    )
+
+    while run.status == "in_progress":
+        run = ai21_client.beta.threads.runs.retrieve(thread_id=thread.id, run_id=run.id)
+
+    if run.status == "completed":
+        messages = ai21_client.beta.threads.messages.list(thread_id=thread.id)
+        print("Messages:")
+        print("\n".join(f"{msg.role}: {msg.content['text']}" for msg in messages.results))
+
+
+if __name__ == "__main__":
+    main()
---
+++ examples/studio/assistant/async_assistant.py
@@ -0,0 +1,42 @@
+import asyncio
+
+from ai21 import AsyncAI21Client
+
+
+async def main():
+    ai21_client = AsyncAI21Client(
+        api_key="413NQymWvgp83hNaqbA3EwYgqUjvREgn", api_host="https://api-stage.ai21.com/studio/v1"
+    )
+
+    assistant = await ai21_client.beta.assistants.create(name="My Assistant")
+
+    thread = await ai21_client.beta.threads.create(
+        messages=[
+            {
+                "role": "user",
+                "content": {
+                    "type": "text",
+                    "text": "Hello",
+                },
+            },
+        ]
+    )
+
+    run = await ai21_client.beta.threads.runs.create(
+        thread_id=thread.id,
+        assistant_id=assistant.id,
+        description="Get response",
+        optimization="latency",
+    )
+
+    while run.status == "in_progress":
+        run = await ai21_client.beta.threads.runs.retrieve(thread_id=thread.id, run_id=run.id)
+
+    if run.status == "completed":
+        messages = await ai21_client.beta.threads.messages.list(thread_id=thread.id)
+        print("Messages:")
+        print("\n".join(f"{msg.role}: {msg.content['text']}" for msg in messages.results))
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
