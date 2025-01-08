c09170fdc2ce93aa768704aa343c769af654d686
Ben <23212294+benshuk@users.noreply.github.com>
2025-01-08 12:52:22 +0200

    refactor: :label: change `rag` tool resource to `file_search` (#251)
    Co-authored-by: benshuk <bens@ai21.com>

--- ai21/models/assistant/assistant.py
+++ ai21/models/assistant/assistant.py
@@ -3,10 +3,10 @@ from typing import Optional, Literal
 from typing_extensions import TypedDict
 
 Optimization = Literal["cost", "latency"]
-Tool = Literal["rag", "web_search", "plan_approval"]
+Tool = Literal["file_search", "web_search", "plan_approval"]
 
 
 class ToolResources(TypedDict, total=False):
-    rag: Optional[dict]
+    file_search: Optional[dict]
     web_search: Optional[dict]
     plan_approval: Optional[dict]
