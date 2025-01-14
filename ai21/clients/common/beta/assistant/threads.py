7198db963dd9d922ec290ac5f9e79b941a116075
benshuk <bens@ai21.com>
2025-01-14 18:16:01 +0200

    chore: :recycle: thread messages should be optional

--- ai21/clients/common/beta/assistant/threads.py
+++ ai21/clients/common/beta/assistant/threads.py
@@ -1,11 +1,13 @@
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
-from typing import List
+from typing import List, Optional
 
 from ai21.clients.common.beta.assistant.messages import BaseMessages
-from ai21.models.assistant.message import Message
+from ai21.models.assistant.message import Message, modify_message_content
 from ai21.models.responses.thread_response import ThreadResponse
+from ai21.types import NOT_GIVEN, NotGiven
+from ai21.utils.typing import remove_not_given
 
 
 class BaseThreads(ABC):
@@ -15,10 +17,18 @@ class BaseThreads(ABC):
     @abstractmethod
     def create(
         self,
-        messages: List[Message],
+        messages: List[Message] | NotGiven = NOT_GIVEN,
         **kwargs,
     ) -> ThreadResponse:
         pass
+
+    def _create_body(self, messages: List[Message] | NotGiven, **kwargs) -> Optional[dict]:
+        body = remove_not_given({"messages": messages, **kwargs})
+
+        if "messages" in body:
+            body["messages"] = [modify_message_content(message) for message in body["messages"]]
+
+        return body
 
     @abstractmethod
     def retrieve(self, thread_id: str) -> ThreadResponse:
