b0104f017653d136802068272d6ab6547b9f5341
benshuk <bens@ai21.com>
2025-03-13 11:15:58 +0200

    fix: :fire: remove unused functions

--- ai21/clients/common/maestro/run.py
+++ ai21/clients/common/maestro/run.py
@@ -1,12 +1,9 @@
 from __future__ import annotations
 
-import inspect
 from abc import ABC, abstractmethod
-from typing import List, Dict, Any, get_origin, get_args, Optional
+from typing import List, Dict, Any
 
-from pydantic import BaseModel
-
-from ai21.models._pydantic_compatibility import _to_dict, _to_schema
+from ai21.models._pydantic_compatibility import _to_dict
 from ai21.models.chat import ChatMessage
 from ai21.models.maestro.run import (
     Tool,
@@ -14,55 +11,10 @@ from ai21.models.maestro.run import (
     RunResponse,
     DEFAULT_RUN_POLL_INTERVAL,
     DEFAULT_RUN_POLL_TIMEOUT,
-    OutputType,
     Requirement,
 )
 from ai21.types import NOT_GIVEN, NotGiven
 from ai21.utils.typing import remove_not_given
-
-
-def __primitive_to_schema(output_type):
-    type_mapping = {
-        int: "integer",
-        float: "number",
-        str: "string",
-        bool: "boolean",
-    }
-
-    if output_type in type_mapping:
-        return {"type": type_mapping[output_type]}
-
-    origin = get_origin(output_type)
-    args = get_args(output_type)
-
-    if origin is list and len(args) == 1 and args[0] in type_mapping:
-        return {"type": "array", "items": {"type": type_mapping[args[0]]}}
-
-
-def __output_type_to_json_schema(output_type: OutputType | NotGiven) -> Optional[Dict[str, Any]]:
-    if not output_type or isinstance(output_type, NotGiven):
-        return NOT_GIVEN
-
-    if inspect.isclass(output_type) and issubclass(output_type, BaseModel):
-        result = _to_schema(output_type)
-    else:
-        result = __primitive_to_schema(output_type)
-
-    if not result:
-        raise ValueError(
-            "Unsupported output type. Supported types are:\n"
-            "- Pydantic model\n"
-            "- str\n"
-            "- int\n"
-            "- float\n"
-            "- bool\n"
-            "- List[str]\n"
-            "- List[int]\n"
-            "- List[float]\n"
-            "- Valid JSON schema dict"
-        )
-
-    return result
 
 
 class BaseMaestroRun(ABC):
