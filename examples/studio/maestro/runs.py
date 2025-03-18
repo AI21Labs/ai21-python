df840314f332ef18dd571602b4e0fcbb7ce33c31
benshuk <bens@ai21.com>
2025-03-18 16:52:48 +0200

    docs: :memo: update examples

--- examples/studio/maestro/runs.py
+++ examples/studio/maestro/runs.py
@@ -5,8 +5,17 @@ client = AI21Client()
 
 def main():
     run_result = client.beta.maestro.runs.create_and_poll(
-        input="Analyze the text below and determine who's the best pokemon ever",
-        context={"text": "Psyduck is the best pokemon."},
+        input="Write a poem about the ocean",
+        requirements=[
+            {
+                "name": "length requirement",
+                "description": "The length of the poem should be less than 1000 characters",
+            },
+            {
+                "name": "rhyme requirement",
+                "description": "The poem should rhyme",
+            },
+        ],
     )
 
     print(run_result)
