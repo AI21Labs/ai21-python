1ec419d6fe073550a47f1556ef0edb9d56b306e7
michals <michals@ai21.com>
2025-04-28 10:17:07 +0300

    fix: fix test

--- tests/integration_tests/clients/studio/test_maestro.py
+++ tests/integration_tests/clients/studio/test_maestro.py
@@ -4,13 +4,13 @@ from ai21 import AsyncAI21Client
 
 
 @pytest.mark.asyncio
-async def test_maestro__when_upload__should_return_data_sources(file_in_library: str):
+async def test_maestro__when_upload__should_return_data_sources():  # file_in_library: str):
     client = AsyncAI21Client()
     result = await client.beta.maestro.runs.create_and_poll(
         input="When did Einstein receive a Nobel Prize?", tools=[{"type": "file_search"}], include=["data_sources"]
     )
-    assert result["status"] == "completed", "Expected 'completed' status"
-    assert result["result"], "Expected a non-empty answer"
-    assert result["data_sources"], "Expected data sources"
-    assert len(result["data_sources"]["file_search"]) > 0, "Expected at least one file search data source"
-    assert result["data_sources"]["web_search"] is None, "Expected no web search data sources"
+    assert result.status == "completed", "Expected 'completed' status"
+    assert result.result, "Expected a non-empty answer"
+    assert result.data_sources, "Expected data sources"
+    assert len(result.data_sources["file_search"]) > 0, "Expected at least one file search data source"
+    assert result.data_sources.get("web_search") is None, "Expected no web search data sources"
