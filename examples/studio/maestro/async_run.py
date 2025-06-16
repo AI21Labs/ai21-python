cd8bc036d4e354846cd28229bea334d1325282f1
bennyai21 <168088189+bennyai21@users.noreply.github.com>
2025-06-16 11:01:18 +0300

    fix: raise error when poll_timeout_sec is reached (#296)
    * fix: raise error when poll_timeout_sec is reached
    
    * chore: handle timeout errors in async_run and run examples
    
    ---------
    
    Co-authored-by: benshuk <bens@ai21.com>

--- examples/studio/maestro/async_run.py
+++ examples/studio/maestro/async_run.py
@@ -6,22 +6,25 @@ client = AsyncAI21Client()
 
 
 async def main():
-    run_result = await client.beta.maestro.runs.create_and_poll(
-        input="Write a poem about the ocean",
-        requirements=[
-            {
-                "name": "length requirement",
-                "description": "The length of the poem should be less than 1000 characters",
-            },
-            {
-                "name": "rhyme requirement",
-                "description": "The poem should rhyme",
-            },
-        ],
-        include=["requirements_result"],
-    )
+    try:
+        run_result = await client.beta.maestro.runs.create_and_poll(
+            input="Write a poem about the ocean",
+            requirements=[
+                {
+                    "name": "length requirement",
+                    "description": "The length of the poem should be less than 1000 characters",
+                },
+                {
+                    "name": "rhyme requirement",
+                    "description": "The poem should rhyme",
+                },
+            ],
+            include=["requirements_result"],
+        )
 
-    print(run_result)
+        print(run_result)
+    except TimeoutError:
+        print("The run timed out")
 
 
 if __name__ == "__main__":
--- examples/studio/maestro/run.py
+++ examples/studio/maestro/run.py
@@ -4,22 +4,25 @@ client = AI21Client()
 
 
 def main():
-    run_result = client.beta.maestro.runs.create_and_poll(
-        input="Write a poem about the ocean",
-        requirements=[
-            {
-                "name": "length requirement",
-                "description": "The length of the poem should be less than 1000 characters",
-            },
-            {
-                "name": "rhyme requirement",
-                "description": "The poem should rhyme",
-            },
-        ],
-        include=["requirements_result"],
-    )
+    try:
+        run_result = client.beta.maestro.runs.create_and_poll(
+            input="Write a poem about the ocean",
+            requirements=[
+                {
+                    "name": "length requirement",
+                    "description": "The length of the poem should be less than 1000 characters",
+                },
+                {
+                    "name": "rhyme requirement",
+                    "description": "The poem should rhyme",
+                },
+            ],
+            include=["requirements_result"],
+        )
 
-    print(run_result)
+        print(run_result)
+    except TimeoutError:
+        print("The run timed out")
 
 
 if __name__ == "__main__":
