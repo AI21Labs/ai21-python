c7749aef9a0ca2b50d7cee7898d3597242dbb36f
Moti Tenzer <motitenzer@Moti-Tenzer-D269D07MXK.local>
2025-11-12 15:29:43 +0200

    Update Maestro example to demonstrate AI21 info retrieval

--- examples/studio/maestro/run.py
+++ examples/studio/maestro/run.py
@@ -1,24 +1,27 @@
 from ai21 import AI21Client
-
-client = AI21Client()
+from ai21.models.chat import ChatMessage
+client = AI21Client(api_key="your_API_key")
 
 
 def main():
     run_result = client.beta.maestro.runs.create_and_poll(
-        input="Write a poem about the ocean",
+        input="Tell me about AI21 Maestro",
         requirements=[
             {
                 "name": "length requirement",
-                "description": "The length of the poem should be less than 1000 characters",
+                "description": "The length of the response should be less than 2000 characters",
             },
             {
-                "name": "rhyme requirement",
-                "description": "The poem should rhyme",
+                "name": "source requirement",
+                "description": "Should rely on information from these websites: https://www.ai21.com/, https://www.ai21.com/maestro/, https://docs.ai21.com/home",
             },
         ],
+        include=["requirements_result"]
     )
 
-    print(run_result)
+    print("\n=== AI21 MAESTRO INFO ===")
+    print(run_result.result)
+    print()
 
 
 if __name__ == "__main__":
