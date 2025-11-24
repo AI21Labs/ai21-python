aa6fa389e677fb678607e4980d611c8e421e2c29
ilanm <ilanm@ai21.com>
2025-11-24 16:37:57 +0200

    fix: format

--- examples/studio/maestro/async_run.py
+++ examples/studio/maestro/async_run.py
@@ -8,26 +8,27 @@ client = AsyncAI21Client()
 async def main():
     try:
         run_result = await client.beta.maestro.runs.create_and_poll(
-        input="Tell me about AI21 Maestro",
-        requirements=[
-            {
-                "name": "length requirement",
-                "description": "The length of the response should be less than 2000 characters",
-            },
-            {
-                "name": "source requirement",
-                "description": (
-                    "Should rely on information from these websites: "
-                    "https://www.ai21.com/, https://www.ai21.com/maestro/, "
-                    "https://docs.ai21.com/home"
-                ),
-            },
-        ],
-        include=["requirements_result"]
-    )
-        print(run_result.result)        
+            input="Tell me about AI21 Maestro",
+            requirements=[
+                {
+                    "name": "length requirement",
+                    "description": "The length of the response should be less than 2000 characters",
+                },
+                {
+                    "name": "source requirement",
+                    "description": (
+                        "Should rely on information from these websites: "
+                        "https://www.ai21.com/, https://www.ai21.com/maestro/, "
+                        "https://docs.ai21.com/home"
+                    ),
+                },
+            ],
+            include=["requirements_result"],
+        )
+        print(run_result.result)
     except TimeoutError:
         print("The run timed out")
+
 
 if __name__ == "__main__":
     asyncio.run(main())
--- examples/studio/maestro/run.py
+++ examples/studio/maestro/run.py
@@ -6,22 +6,22 @@ client = AI21Client()
 def main():
     try:
         run_result = client.beta.maestro.runs.create_and_poll(
-        input="Tell me about AI21 Maestro",
-        requirements=[
-            {
-                "name": "length requirement",
-                "description": "The length of the response should be less than 2000 characters",
-            },
-            {
-                "name": "source requirement",
-                "description": (
-                    "Should rely on information from these websites: "
-                    "https://www.ai21.com/, https://www.ai21.com/maestro/, "
-                    "https://docs.ai21.com/home"
-                ),
-            },
-        ],
-        include=["requirements_result"]
+            input="Tell me about AI21 Maestro",
+            requirements=[
+                {
+                    "name": "length requirement",
+                    "description": "The length of the response should be less than 2000 characters",
+                },
+                {
+                    "name": "source requirement",
+                    "description": (
+                        "Should rely on information from these websites: "
+                        "https://www.ai21.com/, https://www.ai21.com/maestro/, "
+                        "https://docs.ai21.com/home"
+                    ),
+                },
+            ],
+            include=["requirements_result"],
         )
         print(run_result)
     except TimeoutError:
